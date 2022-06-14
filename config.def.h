4abe01b5858937a42279b056cf63ba95d01a7793
Bartłomiej Broniecki <me@br0n.xyz>
2022-06-15 00:07:50 +0200

    add more fonts

--- config.def.h
+++ config.def.h
@@ -6,7 +6,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font2[] = { "JoxPixels:pixelsize=10:antialias=true:autohint=true", 
+                         "Siji:pixelsize=10:antialias=true:autohint=true" };
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
